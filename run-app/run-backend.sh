4f4a7ee8d9f363f4707b9557ae1dd1f703e5c7c7
nar <nvasquez@virtuales.io>
2021-09-11 18:40:44 -0500

    Run the app

--- run-app/run-backend.sh
+++ run-app/run-backend.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
-echo "Hello from backend!"
+
+# Executing bin
+cd /shared
+chmod +x vuego-demoapp
+./vuego-demoapp
+echo "=====**bin executed**====="
+
+sudo sh -c "echo export PORT=4001 >> /etc/profile.d/sh.local"
+source /etc/profile.d/sh.local
+
+# Running on background
+cd /vagrant/run-app
+./run-backend.sh < /dev/null >& /dev/null &
+disown
+exit
